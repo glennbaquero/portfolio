a8846f5194c559f79cf280d81a329513f621c205
Glenn Baquero <baqueroglenncarlo@gmail.com>
2026-01-06 01:24:28 +0800

    added file for tailwind config and added projects

---
+++ js/tailwind-config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#00adb5',
+                secondary: '#00d9ff',
+                dark: '#0a0a0f',
+                darkalt: '#1a1a2e',
+            }
+        }
+    }
+}
